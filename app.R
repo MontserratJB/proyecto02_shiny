936adaf41ef500210fcfdaa5db8f0140d086d354
MontserratJB <montsejimenezb@gmail.com>
2021-11-27 07:19:02 -0600

    Carga de via primaria

---
+++ app.R
@@ -0,0 +1,34 @@
+# Paquetes
+library(dplyr)
+library(sf)
+library(terra)
+library(raster)
+library(rgdal)
+library(DT)
+library(plotly)
+library(leaflet)
+library(leaflet.providers)
+library(leaflet.extras)
+library(shiny)
+library(shinydashboard)
+
+# Lectura de una capa vectorial (GeoJSON) de distritos de Montes de Oca
+distritos <-
+  st_read(
+    "https://raw.githubusercontent.com/MontserratJB/Proyecto-2021/master/Distritos.geojson",
+    quiet = TRUE
+  )
+
+# Lectura de un archivo CSV con datos de Presupuestos participativos en Montes de Oca
+presupuesto <-
+  st_read(
+    "/vsicurl/https://raw.githubusercontent.com/MontserratJB/tarea03-shinyapp/master/Pres_part.csv",
+    options = c(
+      "X_POSSIBLE_NAMES=decimalLongitude",
+      "Y_POSSIBLE_NAMES=decimalLatitude"
+    ),
+    quiet = TRUE
+  )
+
+# Asignación de un CRS al objeto presupuesto
+st_crs(presupuesto) <- 4326
